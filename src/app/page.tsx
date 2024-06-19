b2c292eafb2b1c6baec78307012beece4de05ee8
aravindinduri <thearavindinduri@gmail.com>
2024-06-19 09:48:49 +0530

    added changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
+"use client";
+import { motion } from "framer-motion"
+import { Example } from "@/components/Example";
 export default function Home() {
   return (
-   <h1>
-    Hello world
-   </h1>
+    <Example/>
   );
 }
